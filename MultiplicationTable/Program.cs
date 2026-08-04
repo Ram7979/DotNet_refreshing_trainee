98387cf8c67e696c5eee114903e76b80474c9214
Ram7979 <118529741+Ram7979@users.noreply.github.com>
2026-08-04 09:54:51 +0530

    codes

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+    public int[] MultipliTable(int n, int upto)
+    {
+        int[] arr = new int[upto];
+        for (int i = 1; i <= upto; i++)
+        {
+            arr[i - 1] = n * i;
+        }
+        return arr;
+
+    }
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+        int n = int.Parse(Console.ReadLine());
+        int upto = int.Parse(Console.ReadLine());
+
+        int[] res = p.MultipliTable(n, upto);
+        Console.WriteLine(string.Join(",", res));
+
+    }
+}
---
+++ swapnumber/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program{
+    public void swapNumbers(ref int a, ref int b){
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine("a = "+a);
+        Console.WriteLine("b = "+b);
+    }
+    public static void Main(String[] args){
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Program p = new Program();
+        Console.WriteLine("After wapping: ");
+        p.swapNumbers(ref a, ref b);
+    }
+}
